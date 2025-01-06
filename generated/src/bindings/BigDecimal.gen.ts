b271aad061bddab1cf977b03f4c8c684d2c3fcf9
rafi <96052022+rersozlu@users.noreply.github.com>
2025-01-06 14:28:11 +0300

    Delete generated directory

--- generated/src/bindings/BigDecimal.gen.ts
+++
@@ -1,8 +0,0 @@
-/* TypeScript file generated from BigDecimal.res by genType. */
-
-/* eslint-disable */
-/* tslint:disable */
-
-import type {default as $$t} from 'bignumber.js';
-
-export type t = $$t;
